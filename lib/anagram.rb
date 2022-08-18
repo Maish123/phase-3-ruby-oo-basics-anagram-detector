3efa86760c2ee6b9347c82c7dd49fa18eaa2d082
Denis Maina <Denis.maina@student.moringaschool.com>
2022-08-18 18:01:47 +0300

    commit tree 16aebdf5498a8bf5466bd2ecba8955e8856271e7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,36 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word=word
+    end
+
+    def match(string)
+        word_normal = @word.chars
+        word_reverse = string.chars.reverse
+        if word_normal.sort == word_reverse.sort
+            string.reverse
+        else
+            []
+        end
+    end
+end
+
+anagram1 = Anagram.new("listen")
+pp anagram1.match(%w[enlists])
+
+word = "denis"
+
+def checker(word)
+    char_normal = word.chars
+    char_reverse = word.chars.reverse
+    if char_normal.sort == char_reverse.sort
+        word.reverse
+    else
+        nil
+    end
+end
+
+worddie = checker("denis")
+
+puts worddie
